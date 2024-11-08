51e6548d5e55c7173cc5ceb02ee6c519d87dad56
Esdras Ntuyenabo <esdrasntuyenabo40@gmail.com>
2024-11-08 08:57:37 -0500

    set up _app (basic display)

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,10 +1,32 @@
 /* eslint-disable react/jsx-props-no-spreading */
 import PropTypes from "prop-types";
 import "@/styles/globals.css";
+import PropTypes from "prop-types";
+import { useRouter } from "next/router";
+import Head from "next/head";
+import { Main } from "next/document";
+import { Component } from "react";
 
-export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+function MainApp({ Component, pageProps }) {
+
+  return (
+    <div>
+      <Head>
+        <title>
+          Harmonize
+        </title>
+      </Head>
+      <Main>
+        <h1 className="title"> Harmonize </h1>
+        <Component {...pageProps} />
+      </Main>
+      <footer>copyright @ middlebury college</footer>
+    </div>
+  )
+
 }
+
+export default MainApp;
 
 App.propTypes = {
   Component: PropTypes.elementType.isRequired,
