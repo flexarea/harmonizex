4a8388b3cf741143a7225d39ce038c0876f05d58
alexnicholsamu <alexander.k.nichols@gmail.com>
2024-11-19 12:07:52 -0500

    pls

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,8 +1,13 @@
 import '../styles/globals.css';  // Import global CSS
 import PropTypes from 'prop-types';
+import { SessionProvider } from 'next-auth/react';
 
-function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+function MyApp({ Component, pageProps: { session, ...pageProps } }) {
+  return (
+    <SessionProvider session={session}>
+      <Component {...pageProps} />
+    </SessionProvider>
+  )
 }
 
 MyApp.propTypes = {
