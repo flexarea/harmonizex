2d1ffa8c6fc3a74b3862da03fa213abe16d177f0
Esdras Ntuyenabo <esdrasntuyenabo40@gmail.com>
2024-12-10 15:47:32 -0500

    added fetch candidate handler (not tested)

--- src/pages/api/getCandidates.js
+++ src/pages/api/getCandidates.js
@@ -1,13 +1,73 @@
 import hardFilter from "../../services/candidatesFinder";
+import { knex } from "../../../knex/knex";
+import { createRouter } from "next-connect";
+import { getServerSession } from "next-auth/session";
+import { authOptions } from "./auth/[...nextauth]";
+import { authenticated } from "../../lib/middleware";
 
-export default async function handler(req, res) {
+const router = createRouter()
+const session = await getServerSession(req, res, authOptions)
+
+router.get(authenticated, async (req, res) => {
   try {
-    const userId = req.query.userId || 1; // Replace with actual user ID
-    // const minScore = req.query.minScore || 3;
-    const candidates = await hardFilter(userId);
-    res.status(200).json({ candidates });
+    const userPreference = await knex("User")
+      .where("user_id", session.user.id)
+      .select("prefer_men", "prefer_women", "prefer_enby")
+      .first()
+    const setUpCandidatePreference = () => {
+      let tempBuffer = []
+      for (const [key, value] of Object.entries(userPreference)) {
+        if (value) {
+          switch (key) {
+            case 'prefer_men': {
+              tempBuffer.push('prefer_women')
+              break;
+            }
+            case 'prefer_women': {
+              tempBuffer.push('prefer_men')
+              break;
+            }
+            case 'prefer_enby': {
+              tempBuffer.push('prefer_enby')
+              break;
+            }
+
+            default: {
+              return null
+            }
+          }
+        }
+      }
+      return tempBuffer
+    }
+
+    const candidatePreference = setUpCandidatePreference()
+
+    const users = await knex("User")
+      .whereNot("user_id", session.user.id)
+      .limit(10)
+      .select()
+    const getMatchingCandidate = () => {
+      return users.filter((user) => {
+        for (const [key, value] of Object.entries(user)) {
+          if (['prefer_men', 'prefer_women', 'prefer_enby'].includes(key)) {
+            if (value && candidatePreference.includes(key)) {
+              return true //found a match
+            }
+          }
+          return false //no match 
+        }
+      })
+    }
+
+    const candidates = getMatchingCandidate();
+    if (candidates) {
+      res.status(200).json({ data })
+    }
   } catch (error) {
-    console.error("Error fetching candidates:", error);
-    res.status(500).json({ error: "Internal Server Error" });
+    res.status(500).json({ error: error.message })
   }
-}
+})
+
+export default router.handler();
+
